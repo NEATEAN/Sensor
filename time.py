cc62557af1b566b912838b8a8d5997a63ff21e9e
NEATEAN <34091204+NEATEAN@users.noreply.github.com>
2018-05-31 12:29:03 +0000

    Update time.py

--- time.py
+++ time.py
@@ -1,3 +1,36 @@
+# This Python file uses the following encoding:utf-8
+
+import RPi.GPIO as GPIO
+import sys
+import time
+import Adafruit_DHT
+import spidev
+import pymysql
+
+sensor = Adafruit_DHT.DHT11
+
+pin=2
+adcchannel=0
+space=1
+
+spi.spidev.SpiDev()
+spi.open(0,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
